6fcff4516071c387356b4bccd6f793c65cd8ab86
James ELkins <james.elkins1@gmail.com>
2015-10-26 19:25:30 -0700

    amend add google url parsing file and more readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dating_science
+
+here's the plan:
+go use google custom search api to get top 10 results for an input name:
+https://developers.google.com/custom-search/docs/overview
+prob use sinatra
+
+prob use https://github.com/google/google-api-ruby-client/
+prob use https://github.com/sferik/twitter
+
+then see if any of those links are from facebook, twitter, linkedin, or github
+if any of them are, go to that page & scrape it & get the top 10 most used words, excluding words from the vendor
+then output those words to a webpage
--- google_url_parsing.R
+++ google_url_parsing.R
@@ -15,35 +15,35 @@ library(RColorBrewer)
 
 googleURLs <- function(search) {
   # read in page contents
-#  search="mike pinkowish san francisco"
+#  search="bart simpson san francisco"
   search_str = str_replace_all(search, " ", "+")
   u <- paste0("http://www.google.com/search?aq=f&gcx=w&sourceid=chrome&ie=UTF-8&q=", search_str)
-  
+
   html <- getURL(u)
   class(html)
-  
+
   # parse HTML into tree structure
   doc <- htmlParse(html)
-  
+
   # extract url nodes using XPath. Originally I had used "//a[@href][@class='l']" until the google code change.
   attrs <- xpathApply(doc, "//h3//a[@href]", xmlAttrs)
-  
+
   # extract urls
   links <- sapply(attrs, function(x) x[[1]])
-  
+
   # free doc from memory
   free(doc)
-  
+
   # ensure urls start with "http" to avoid google references to the search page
   links <- grep("http", links, fixed = TRUE, value=TRUE)
   links = sapply(links, function(l) str_replace_all(l, "/url\\?q=", ""))
   links = as.character(links)
-  
+
   links=sapply(links, function(l) strsplit(l[1], split="&sa")[[1]][1])
   links=sapply(links, function(l) strsplit(l[1], split="%3")[[1]][1])
-  
+
   links = as.character(links)
-  
+
   return(links)
 }
 
@@ -57,53 +57,53 @@ getURLTexts = function(search) {
     print(u)
     html_page=NULL
     html_page=try(read_html(paste0(u, ".html")))
-    
+
     if(is.null(html_page)) {
       print(".html didn't work")
       html_page=try(read_html(u))
-      
+
     }
-    
-    
+
+
     if(!is.null(html_page)) {
       doc=htmlTreeParse(html_page, useInternal=TRUE)
       doc = unlist(xpathApply(doc, '//p', xmlValue))
-      
+
       # Replace all \n by spaces
       doc = gsub('\\n', ' ', doc)
-      
+
       # Join all the elements of the character vector into a single
       # character string, separated by spaces
       doc = paste(doc, collapse = ' ')
       str_vec = c(str_vec, doc)
       current_name = u
       if(str_detect(u, "twitter")) {
-        current_name="twitter"  
+        current_name="twitter"
       }
       if(str_detect(u, "linkedin")) {
-        current_name="linkedin"  
+        current_name="linkedin"
       }
       if(str_detect(u, "github")) {
-        current_name="github"  
+        current_name="github"
       }
       if(str_detect(u, "facebook")) {
-        current_name="facebook"  
+        current_name="facebook"
       }
       if(str_detect(u, "quora")) {
-        current_name="quora"  
+        current_name="quora"
       }
       if(str_detect(u, "google")) {
-        current_name="google"  
+        current_name="google"
       }
       names_str=c(names_str, current_name)
-      
+
     }
 
   }
   names(str_vec)=names_str
   return(str_vec)
 }
-search="mike chambers san francisco"
+search="bart simpson san francisco"
 text = getURLTexts(search)
 
 
